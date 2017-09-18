c0ea782a4b165452452acefcf285c3b5e8ad0791
Tauqeer <tauqeerkethwal@gmail.com>
2017-09-18 17:59:39 +0200

    Adding keys module

--- Store.Web/Areas/Admin/Controllers/CustomerController.cs
+++ Store.Web/Areas/Admin/Controllers/CustomerController.cs
@@ -1,9 +1,20 @@
 ﻿using Freelancer.Model.Models.CustomerKeys;
 using Freelancer.Web.Areas.Admin.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
 namespace Freelancer.Web.Areas.Admin.Controllers
 {
+    public class SelectedInterventionsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public System.DateTime ToDoBefore { get; set; }
+
+        public System.DateTime PlannedDate { get; set; }
+    }
     public class CustomerController : Controller
     {
         // GET: Admin/Customer
@@ -11,17 +22,28 @@ namespace Freelancer.Web.Areas.Admin.Controllers
         {
             CustomerViewModel cv = new CustomerViewModel();
             List<CustomerKeys> li = new List<CustomerKeys>();
-            li.Add(new CustomerKeys { Name = "0fa", CustomerId = "0" });
+            li.Add(new CustomerKeys { Name = "0fa", CustomerId = "0", KeyId = 0 });
 
             cv.CustomerKeysList = li;
 
             return View(cv);
         }
-
-        public ActionResult AddObject(int index)
+        [HttpPost]
+        public ActionResult AddObject(int index, List<CustomerKeys> CustomerKeysList)
         {
-            return PartialView("_EmptyRow", new CustomerKeys { CustomerId = index.ToString() });
+            Tuple<List<CustomerKeys>, int> tuple = new Tuple<List<CustomerKeys>, int>(CustomerKeysList, index);
+            return PartialView("_EmptyRow", tuple);
         }
+        [HttpPost]
+        public ActionResult RemoveObject(int index, List<CustomerKeys> CustomerKeysList)
+        {
+            Tuple<List<CustomerKeys>, int> tuple = new Tuple<List<CustomerKeys>, int>(CustomerKeysList, index);
+            return PartialView("_DeleteRow", tuple);
+        }
+        //public ActionResult AddObject(int index)
+        //{
+        //    return PartialView("_EmptyRow", new CustomerKeys { CustomerId = index.ToString() });
+        //}
         [HttpPost]
         public ActionResult Index(CustomerViewModel cv)
         {
