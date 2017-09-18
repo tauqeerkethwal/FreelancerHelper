34bdedad76c48d27fcf1c79b4f9da87af110f544
Tauqeer <tauqeerkethwal@gmail.com>
2017-09-18 21:36:45 +0200

    Udate system.web.mvc and customerpet etc

---
+++ Store.Service/CustomerPetService.cs
@@ -0,0 +1,51 @@
+﻿
+using Freelancer.Data.Infrastructure;
+using Freelancer.Data.Repositories;
+using Freelancer.Model.Models.CustomerPet;
+using System;
+using System.Collections.Generic;
+
+namespace Freelancer.Service
+{
+    public interface ICustomerPetService
+    {
+
+
+
+        IEnumerable<CustomerPet> GetPetsByCustomerId(Guid Id);
+        void CreateCustomerPet(CustomerPet customerPet);
+        void SaveCategory();
+    }
+
+    public class CustomerPetService : ICustomerPetService
+    {
+        private readonly ICustomerPetRepository customerPetRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public CustomerPetService(ICustomerPetRepository customerPetRepository, IUnitOfWork unitOfWork)
+        {
+            this.customerPetRepository = customerPetRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        #region 
+
+        public IEnumerable<CustomerPet> GetPetsByCustomerId(Guid Id)
+        {
+            var animalsByEmployee = customerPetRepository.GetPetsByCustomerId(Id);
+            return animalsByEmployee;
+        }
+
+        public void CreateCustomerPet(CustomerPet customerPet)
+        {
+            customerPetRepository.Add(customerPet);
+        }
+
+        public void SaveCategory()
+        {
+            unitOfWork.Commit();
+        }
+
+        #endregion
+    }
+}
