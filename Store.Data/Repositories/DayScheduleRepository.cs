76b61395db4a638adb81960d52d077f0053b2af0
Tauqeer <tauqeerkethwal@gmail.com>
2018-05-30 01:13:56 +0200

    add dayschedule repo and service etc

---
+++ Store.Data/Repositories/DayScheduleRepository.cs
@@ -0,0 +1,51 @@
+﻿using Freelancer.Data.Infrastructure;
+using Freelancer.Model.Models.Schedule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Freelancer.Data.Repositories
+{
+    public interface IDayScheduleRepository : IRepository<DaySchedule>
+    {
+
+        IEnumerable<DaySchedule> GetDayScheduleByScheduleId(Guid ScheduleId);
+        void DeleteDaySchedule(Guid ScheduleId);
+        void CreateDayScheudle(DaySchedule daySchedule);
+
+    }
+    public class DayScheduleRepository : RepositoryBase<DaySchedule>, IDayScheduleRepository
+    {
+        public DayScheduleRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
+
+        }
+
+        public IEnumerable<DaySchedule> GetDayScheduleByScheduleId(Guid ScheduleId)
+        {
+            var DaySchedule = this.DbContext.DaySchedules.Where(x => x.ScheduleId == ScheduleId && x.del == false);
+            return DaySchedule;
+        }
+        public void CreateDayScheudle(DaySchedule daySchedule)
+        {
+           
+                this.DbContext.DaySchedules.Add(daySchedule);
+           
+
+        }
+        public  void DeleteDaySchedule(Guid ScheduleId)
+        {
+
+            var daySchedule = GetDayScheduleByScheduleId(ScheduleId);
+            foreach (var dSchedule in daySchedule)
+            {
+                dSchedule.del = true;
+                base.Update(dSchedule);
+            }
+
+            
+           
+        }
+    }
+}
---
+++ Store.Service/DayScheduleService.cs
@@ -0,0 +1,52 @@
+﻿using Freelancer.Data.Infrastructure;
+using Freelancer.Data.Repositories;
+using Freelancer.Model.Models.Schedule;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System;
+
+namespace Store.Service
+{
+    public interface IDayScheduleService
+    {
+        void DeletePreviousDaySchedule(Guid ScheduleId);
+        void CreateDayScheudle(List<DaySchedule> daySchedule);
+        IEnumerable<DaySchedule> GetDayScheduleByScheduleId(Guid ScheduleId);
+        void SaveDaySchedule();
+    }
+    public class DayScheduleService : IDayScheduleService
+    {
+        private readonly IDayScheduleRepository _dayScheduleRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public DayScheduleService(IDayScheduleRepository dayScheduleRepository, UnitOfWork unitofWork)
+        {
+            _dayScheduleRepository = dayScheduleRepository;
+            _unitOfWork = unitofWork;
+
+        }
+        public void CreateDayScheudle(List<DaySchedule> daySchedule)
+        {
+            foreach(var dSchedule in daySchedule)
+            {
+                _dayScheduleRepository.CreateDayScheudle(dSchedule);
+            }
+
+        }
+
+        public void DeletePreviousDaySchedule(Guid ScheduleId)
+        {
+            _dayScheduleRepository.DeleteDaySchedule(ScheduleId);
+        }
+
+        public IEnumerable<DaySchedule> GetDayScheduleByScheduleId(Guid ScheduleId)
+        {
+         return    _dayScheduleRepository.GetDayScheduleByScheduleId(ScheduleId);
+        }
+
+        public void SaveDaySchedule()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
